9564f47d40ae5d5b38d799577632d042f1287096
lrzamelo <larissa240897@gmail.com>
2024-10-16 16:02:05 -0300

    upideite

--- src/app/components/header.tsx
+++ src/app/components/header.tsx
@@ -1,8 +1,14 @@
 export default function Header() {
     return(
-        <header className="z-50 h-16 w-full bg-orange-600 flex items-center text-3xl fixed shadow-lg">
-            <div className="mx-5"><img className="w-14" src="/logo.png" alt="" /></div>
-            <div><img className="w-24" src="/CADU.png" alt="" /></div>
+        <header className="z-50 h-16 w-full bg-orange-600 items-center flex flex-initial text-3xl fixed shadow-lg justify-between">
+            <div className="flex flex-rol items-center">
+            <div className="ml-5"><img className="w-14" src="/logo.png" alt="" /></div>
+            <div className="ml-5"><img className="w-24" src="/CADU.png" alt="" /></div>
+            </div>
+            <div className="flex flex-rol items-center">
+            <div className="mr-5"><img className="w-8" src="/profile.png" alt="" /></div>
+            <div className="mr-5"><img className="w-8" src="/menu.png" alt="" /></div>
+            </div>
         </header>
     )
 }
