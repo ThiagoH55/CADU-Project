171283dd5eae33d3e14ba78854b61183575135be
fagnermouradev2 <fmarshmello2004@gmail.com>
2024-10-24 23:47:41 -0300

    Login feito

--- src/app/components/bottons.tsx
+++ src/app/components/bottons.tsx
@@ -1,5 +1,5 @@
 export default function RoundedNextButton() {
     return (
-        <button className="text-3xl rounded-full text-white bg-orange-500 " type="submit" >&#10140; </button>
+        <button className="w-10 aspect-square text-3xl rounded-full text-white bg-orange-500 " type="submit" >&#10140; </button>
     )    
 }
--- src/app/sign-in/page.tsx
+++ src/app/sign-in/page.tsx
@@ -4,10 +4,15 @@ import { useRouter } from "next/navigation"
 import { useState, FormEvent, useEffect } from "react"
 import { z } from "zod"
 import { signIn as nextAuthSignIn } from 'next-auth/react'
+import RoundedNextButton from "../components/bottons"
+import Link from "next/link"
 
 const carouselImages = [
-    'https://c4.wallpaperflare.com/wallpaper/572/819/777/australia-kangaroo-wallpaper-preview.jpg',
-    'https://images7.alphacoders.com/588/thumb-1920-588602.jpg'
+    'carrousel-1.png',
+    'carrousel-2.png',
+    'carrousel-3.png',
+    'carrousel-4.png',
+    'carrousel-5.png'
 ]
 
 export default function SignIn() {
@@ -81,38 +86,46 @@ export default function SignIn() {
 
     return (
         <div className="bg-orange-500 min-h-screen w-full flex">
-            <div className="w-1/3 flex flex-col items-center justify-center">
+            <div className="gap-6 w-1/3 flex flex-col items-center justify-center">
                 <div>
-                    Olá, eu sou uma logo
+                    <img src="LOGO-BRANCA.svg" alt="" />
                 </div>
 
-                <div>
-                    <form onSubmit={handleSubmit} className="bg-white rounded-2xl p-4 min-h-96 min-w-72 space-y-4 text-red-600">
+                <div className="w-full flex justify-center">
+                    <form onSubmit={handleSubmit} className="w-9/12 bg-white rounded-2xl p-8 min-h-96 min-w-80 space-y-4 font-semibold text-gray-600">
+                        <h1 className="text-center font-[family-name:var(--font-be-vietnam)] pb-5 text-4xl text-orange-500">LOGIN</h1>
                         <div className="flex flex-col">
                             <label htmlFor="email">E-mail</label>
-                            <input type="email" name="email" id="email" />
+                            <input className="bg-gray-300 font-medium rounded-xl p-2" type="email" name="email" id="email" />
                         </div>
 
-                        <div className="flex flex-col">
+                        <div className="flex flex-col w-full">
                             <label htmlFor="password">Senha</label>
-                            <input type="password" name="password" id="password" />
-                        </div>
-
+                            <div className="flex gap-2 w-full">
+                                <input className="bg-gray-300 font-medium rounded-xl p-2 flex flex-1" type="password" name="password" id="password" />
+                                <RoundedNextButton />
+                            </div>
+                            <Link className="text-sm mt-2 ml-1 text-blue-500" href={""}>Esqueceu a senha?</Link>
                         {Object.keys(errors).length > 0 && (
                             <div>
                                 {(Object.entries(errors) as []).map((error, index) => (
-                                    <p key={index}>{error[0]} {error[1]}</p>
+                                    <p className="text-left ml-1 mt-4 w-full text-wrap text-red-500 text-sm" key={index}>{error[0]} {error[1]}</p>
                                 ))}
                             </div>
                         )}
+                            <div className="text-center mt-7 text-orange-500 text-sm">
+                                <p>Ainda não possui uma conta?</p>
+                                <Link className="text-sm mt-2 ml-1 text-blue-500 text-sm" href={"/sign-up"}>Cadastra-se</Link>
+                            </div>
+                        </div>
 
-                        <button type="submit">Criar</button>
+
                     </form>
                 </div>
 
             </div>
             <div className="w-2/3 relative">
-                <div className="h-screen w-full bg-cover bg-center bg-no-repeat ease-in duration-300" style={{backgroundImage: `url(${carouselImage})`}}>
+                <div className="h-screen w-full bg-cover bg-center bg-no-repeat transition-width duration-1000 ease-in-out" style={{ backgroundImage: `url(${carouselImage})` }}>
                 </div>
             </div>
         </div>
