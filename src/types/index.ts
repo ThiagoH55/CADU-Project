8ca57cf0c2c4aa298bbfaf76b805aebe771998c0
BhibPH <philipbhib15@gmail.com>
2024-10-25 13:45:26 -0300

    login authetication

--- src/types/index.ts
+++ src/types/index.ts
@@ -1,4 +1,8 @@
+import { usuarios } from "@prisma/client"
+
 export interface State {
     success: boolean
     errors: object
 }
+
+export type UserWithoutPassword = Omit<usuarios, "USR_SENHA" | "USR_ID"> & { id: string }
