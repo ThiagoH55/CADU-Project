08b82f3289b60eb17800516cf3b7df9055661ec1
JuanOly <juancristen88551@gmail.com>
2024-11-01 17:09:29 -0300

    remodeling animal page

--- src/app/animal-register/page.tsx
+++ src/app/animal-register/page.tsx
@@ -13,7 +13,7 @@ export default function AnimalRegister() {
 
                 <div className="flex flex-rol items-center">
 
-                    <button className="flex justify-self-center ml-6 rotate-180 text-4xl text-white"  >&#10140;</button>
+                    <button className="flex justify-self-center ml-6 rotate-180 text-4xl text-white">&#10140;</button>
 
                 </div>
 
@@ -27,18 +27,22 @@ export default function AnimalRegister() {
             </header>
 
             <div className="h-screen w-screen bg-gray-300 flex justify-center items-center ">
-                <div className="bg-white w-4/6 h-4/5 mt-10 drop-shadow-4xl rounded-3xl">
+                <div className="bg-white w-7/10 h-4/6 mt-10 drop-shadow-4xl rounded-3xl">
                     <div className="w-full flex p-5">
-                        <div className="h-full rounded-md font-[family-name:var(--font-be-vietnam)]">
-                            <img className=" items-center rounded-md" src="img-input.svg" alt="" />
+                        <div className=" ml-6 h-full rounded-md font-[family-name:var(--font-be-vietnam)]">
+                            <label htmlFor="animalPicture" className="cursor-pointer">
+                                <img className=" items-center rounded-md " src="img-input.svg" alt="" />
+                            </label>
+
+                            <input type="file" className="hidden" id="animalPicture" accept="image/*" />
                             <p className="flex text-gray-500 text-center justify-center m-3 ">Enviar fotos do <br />animalzinho</p>
                         </div>
 
                         <form className="w-4/6 font-[family-name:var(--font-be-vietnam)]">
-                            <h1 className="text-orange-500 text-center ml-24 text-4xl ">CADASTRO DE ANIMAL</h1>
+                            <h1 className="text-orange-500 text-center ml-16 text-5xl ">CADASTRO DE ANIMAL</h1>
                             <div className="flex flex-row gap-24">
 
-                                <div className="first:mt-5 first:ml-16">
+                                <div className="first:mt-14 first:ml-16">
 
                                     <InsertInfosInput nameLabel="Tipo de animal" nameInputs="tipoAnimal" />
                                     <InsertInfosInput nameLabel="Raça" nameInputs="raca" />
@@ -48,14 +52,14 @@ export default function AnimalRegister() {
                                     <InsertInfosInput nameLabel="Porte" nameInputs="porte" />
                                 </div>
 
-                                <div className="mt-5 ml-5 text-black">
+                                <div className="mt-14 ml-5 text-black">
 
                                     <InsertInfosInput nameLabel="Castração" nameInputs="castracao" />
                                     <InsertInfosInput nameLabel="Cor" nameInputs="cor" />
                                     <InsertInfosInput nameLabel="Localização" nameInputs="localizacao" />
-                                    <label className="text-lg" htmlFor="descricao">Descrição</label>
+                                    <label className="text-2xl" htmlFor="descricao">Descrição</label>
                                     <textarea className="bg-gray-300 px-2 min-w-60 min-h-20 resize-none rounded-md" name="descricao" />
-                                    <button className="flex mt-8 px-5 py-3 justify-self-center rounded-full text-sm text-white bg-orange-500 " type="submit" >CADASTRAR</button>
+                                    <button className="flex mt-10 px-5 py-3 justify-self-center rounded-full text-sm text-white bg-orange-500 " type="submit" >CADASTRAR</button>
 
                                 </div>
 
