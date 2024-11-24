c957a089f7c3f93514fce15b0b08b3dd01d4c57f
JuanOly <juancristen88551@gmail.com>
2024-11-24 18:05:59 -0300

    adding animal data comp

--- src/app/animal/[slug]/page.tsx
+++ src/app/animal/[slug]/page.tsx
@@ -1,6 +1,7 @@
 'use server'
 
 import { getAnimalById } from "@/app/actions"
+import AnimalData from "@/components/animal-data"
 import Header from "@/components/header"
 import { headers } from "next/headers"
 
@@ -11,7 +12,7 @@ export default async function Page({
 }) {
     const slug = (await params).slug
 
-    // const animal = await getAnimalById(slug)
+    const animal = await getAnimalById(slug)
 
     return (
 
@@ -21,21 +22,37 @@ export default async function Page({
             <div className="bg-gray-300 h-screen flex justify-center items-center">
                 <div className="w-10/12 h-4/6 bg-white drop-shadow-3xl rounded-3xl">
 
-                    <div className="flex h-1/2 mx-7 my-7 ">
-                        <div>
-                            <img className="w-2/3 rounded-md" src="https://i.pinimg.com/564x/43/b8/f7/43b8f757efbe31ed48e6875165f3ee5d.jpg" alt="" />
+                    <div className="flex h-1/2 mx-7 my-7">
+                        <div className="w-72">
+                            <img className="rounded-md" src="https://i.pinimg.com/564x/43/b8/f7/43b8f757efbe31ed48e6875165f3ee5d.jpg" alt="" />
                         </div>
 
-                        <div>
-                            <h1 className="text-black text-6xl">Petulancia de capa</h1>
+                        <div className="mx-7">
+                            <h1 className="text-black text-6xl">
 
-                            <span>Dados</span>
+                                {JSON.stringify(animal)}
+
+                            </h1>
+
+                            <div className="my-5 ">
+
+                                <AnimalData/> <AnimalData/>
+                            </div>
+
+
+                            <div className="mt-36">
+                                <h1 className="text-black text-3xl ">Doador:
+                                    <span className="text-orange-500">Thiago Henrique </span> </h1>
+                            </div>
+
                         </div>
+
+
                     </div>
 
-                    <div className="flex mx-5 justify-center">
+                    <div className="flex mx-7 justify-center">
                         <textarea
-                            className="bg-gray-300 w-11/12 h-64 resize-none rounded-md"
+                            className="bg-gray-300 w-full h-56 resize-none rounded-md"
                             name="descricao" />
                     </div>
 
---
+++ src/components/animal-data.tsx
@@ -0,0 +1,7 @@
+export default function AnimalData() {
+    return (
+
+        <span className=" bg-gray-300 rounded-full px-3 py-1 mr-3 drop-shadow-md text-2xl text-black font-[family-name:var(--font-be-vietnam)]" >Adulto</span>
+    
+    )
+}
