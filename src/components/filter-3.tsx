491a90c76450d6faf851ce78f73504229f2b638b
JuanOly <juancristen88551@gmail.com>
2024-11-30 16:07:38 -0300

    filter adjusting

--- src/components/filter-3.tsx
+++ src/components/filter-3.tsx
@@ -1,6 +1,6 @@
 export default function FiltroLocal() {
     return (
-        <div className="flex flex-col items-center mx-16 w-64 min-h-14 bg-white rounded-2xl drop-shadow-md m-8 text-gray-700">
+        <div className="flex flex-col items-center mx-16 w-64 min-h-14 bg-white rounded-2xl drop-shadow-md m-8 text-gray-800">
             <div className="flex p-3">
                 <h1 className="text-2xl font-[family-name:var(--font-be-vietnam)]">LOCALIZAÇÃO</h1>
                 <div className="w-6 mx-1">
