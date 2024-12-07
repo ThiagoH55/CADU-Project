c99e514d2b34c72b72f943a21d53353e790f99fd
JuanOly <juancristen88551@gmail.com>
2024-12-07 14:37:38 -0300

    Adjusting things

--- src/components/insert-infos.tsx
+++ src/components/insert-infos.tsx
@@ -8,7 +8,7 @@ export default function InsertInfosInput(props: inputProps) {
     return (
         <div className="flex flex-col text-wrap mb-4">
             <label className="text-black text-2xl" htmlFor={props.nameInputs}>{props.nameLabel}</label>
-            <input className="bg-gray-300 text-black  max-w-60 rounded-md px-2" type="text" name={ props.nameInputs}/> 
+            <input className="bg-gray-300 text-black  max-w-60 rounded-md p-1 px-2" type="text" name={ props.nameInputs}/> 
 
     </div>
     )    
